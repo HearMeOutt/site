937ba79e5db933c605bc82f45fbf501e53b6db29
HearMeOutt <165026987+HearMeOutt@users.noreply.github.com>
2024-08-19 10:39:39 -0300

    Add files via upload

---
+++ app/public/assets/components/head.php
@@ -0,0 +1,18 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>HearMeOutt</title>
+
+    <!-- !CSS! -->
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/padrao.css">
+
+    <!-- !FONT AWESOME! -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- !Favicon! -->
+    <link rel="shortcut icon" alt="logo HearMeOut" href="assets/imf/favicon.png" type="image/x-icon">
+
+</head>
---
+++ app/public/src/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+    <html lang="pt-br">
+    <?php 
+        include ('../assets/components/head.php')
+    ?>
+    <body>
+        
+    </body>
+</html>
