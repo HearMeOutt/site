7c7542457072c30ac4e5455bc996573247e9e950
Marcus Vinícius Bizerra Moreira <100208378+MarcusVBMoreira@users.noreply.github.com>
2024-08-22 11:09:00 -0300

    Add files via upload

--- app/public/src/introducao.php
+++ app/public/src/introducao.php
@@ -10,5 +10,49 @@
         <?php
             include ('../assets/components/header.php')
         ?>
+        <div class="container">
+            <!-- !Introdução! -->
+            <div class="introducao">
+                <div class="titulo Michroma">
+                    <h1>INTRODUÇÃO A LIBRAS:</h1>
+                </div>
+                <div class="modulos">
+                    <div class="container_modulos">
+                        <div class="card_modulo card1">
+                            <div class="img_card">
+                                <img src="../assets/img/alfabeto.png" alt="Letras do alfabeto">
+                            </div>
+                            <div class="nome_modulo">
+                                <h2 class="Michroma">Alfabeto em libras</h2>
+                            </div>
+                            <div class="btn_view">
+                                <a href="alfabeto_modulo.php" class="visualizar Michroma">VISUALIZAR</a>
+                            </div>
+                        </div>
+                        <div class="card_modulo card2">
+                            <div class="img_card">
+                                <img src="../assets/img/numeros.png" alt="Números">
+                            </div>
+                            <div class="nome_modulo">
+                                <h2 class="Michroma">Números inicias</h2>
+                            </div>
+                            <div class="btn_view">
+                                <a href="numeros_modulo.php" class="visualizar Michroma">VISUALIZAR</a>
+                            </div>
+                        </div>
+                        <div class="card_modulo card3">
+                            <div class="img_card">
+                                <div class="gear">
+                                    <img src="https://assets.codepen.io/6093409/gear.svg.png" alt="an illustration of a gear" />
+                                </div>
+                            </div>
+                            <div class="nome_modulo">
+                                <h2 class="Michroma">Em desenvolvimento</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
